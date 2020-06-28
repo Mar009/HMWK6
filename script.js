74e4605a41a65de2ef6efe0ed8ed4329e9e9c5a1
MMMarz009 <mar0099887@gmail.com>
2020-06-28 14:24:35 -0500

    Changed script load out & began on JS

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// click event on search btn 
+var button=document.getElementsByClassName("btn btn-outline-success my-2 my-sm-0")
+
+$("button" ).on("click", function(){
+    event.preventDefault();
+
+    var search=$(this).type(":submit");
+    var queryURL= "api.openweathermap.org/data/2.5/forecast?id=" + search + "&appid=7e3154185511d5ccdc5d2529706fdf8f"
+
+    // pull from the queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+    }).then (function(response){
+        
+    })
+
+ });
