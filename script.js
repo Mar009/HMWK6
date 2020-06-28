38f5b847aeba38cf7c231c41cb765b071d0362f1
MMMarz009 <mar0099887@gmail.com>
2020-06-28 16:01:54 -0500

    Started to build JS, issue with load script, issue not found

--- script.js
+++ script.js
@@ -13,11 +13,22 @@ $("button" ).on("click", function(){
 
     }).then (function(response){
         // make the general card for the info
+        var cardForCurrent = $("<div>").attr("class","card-header").text("Current weather for "+ response.name);
+        $("#forecast").append(cardForCurrent);
+
+        var cardRow=$("<div>").attr("class", "row no-gutters");
+        cardForCurrent.append(cardRow);
+
+        //imgs for weather
+        var iconsURL= " https://openweathermap.org/img/wn/"+response.weather[0].icon;
+        var divImg= $("<div>").attr("clas", "col-md-5").append($("<img>").attr("src",iconsURL).attr("class","card-img"));
+        
         // have name & date 
-            // temp 
-            // humidity % 
-            // Wind Speed 
-            // UV index 
+        var headCard = $("<div>").attr("class")
+        // temp 
+        // humidity % 
+        // Wind Speed 
+         // UV index 
 
         // make smaller cards for the next 5 days 
             // have dates 
@@ -27,6 +38,11 @@ $("button" ).on("click", function(){
 
     })
 
-    // Save search history 
+    // Save search history append to id=searchesOld
+    function searchHistory(){
+        let searchList = $("<li>").addClass("list-group-item").text(city);
+        $("#searchesOld").append(searchList);
+    }
+    searchHistory();
 
  });
